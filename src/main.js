6f88ad75cce04d734ed97b5bcf6bce8e694d2bd9
B0ek <miguel_boekhold@hotmail.com>
2019-04-15 21:48:34 +0200

    Vue router included in vue-analytics

--- src/main.js
+++ src/main.js
@@ -1,12 +1,13 @@
-import Vue from 'vue'
-import App from './App.vue'
+import Vue from 'vue';
+import App from './App.vue';
 import VeeValidate from 'vee-validate';
-import VueAnalytics from 'vue-analytics'
+import VueAnalytics from 'vue-analytics';
 import router from './router';
 
 Vue.use(VeeValidate);
 Vue.use(VueAnalytics, {
-  id: 'UA-138368011-1'
+  id: 'UA-138368011-1',
+  router
 });
 Vue.config.productionTip = false
 
